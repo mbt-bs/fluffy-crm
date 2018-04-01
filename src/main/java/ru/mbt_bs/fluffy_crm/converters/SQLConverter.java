d9b8802544872705ac0d78495f43f73d182c1d41
Mike Blacktale <mstr.box@gmail.com>
2018-04-01 16:26:30 +0500

    FIX: NPE in SQL converter

--- src/main/java/ru/mbt_bs/fluffy_crm/converters/SQLConverter.java
+++ src/main/java/ru/mbt_bs/fluffy_crm/converters/SQLConverter.java
@@ -3,8 +3,10 @@ package ru.mbt_bs.fluffy_crm.converters;
 public class SQLConverter {
     public static String convertStringToLiker(String src) {
         StringBuilder result = new StringBuilder("%");
-        for (char singleChar : src.toUpperCase().toCharArray()) {
-            result.append(singleChar).append("%");
+        if (src != null && !src.isEmpty()) {
+            for (char singleChar : src.toUpperCase().toCharArray()) {
+                result.append(singleChar).append("%");
+            }
         }
         return result.toString();
     }
